9bb209bcea8994fecad7458945de5653ad202859
apescara <apescara@alumnos.uai.cl>
2018-11-07 13:57:24 -0300

    inicio_conductor
    empece lo de conductor, pero me equivoque y lo estoy haciendo en eel master jiji

---
+++ Capstone5/DALayer/conductorDA.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntitiesLayer;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DALayer
+{
+    public class conductorDA
+    {
+        #region "SINGLETON"
+        private static conductorDA conductor = null;
+        private conductorDA() { }
+        public static conductorDA getInstance()
+        {
+            if (conductor == null)
+            {
+                conductor = new conductorDA();
+            }
+            return conductor;
+        }
+        #endregion
+
+        public bool RegistrarConductor(conductor objCond)
+        {
+            SqlConnection con = null;
+            SqlCommand cmd = null;
+            bool response = false;
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+            return response;
+        }
+    }
+}
---
+++ Capstone5/EntitiesLayer/conductor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer
+{
+    public class conductor
+    {
+        public int idConductor { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int idEmpTransporte { get; set; }
+        public string RUT { get; set; }
+        public string email { get; set; }
+        public int fono { get; set; }
+
+        public conductor() { }
+    }
+}
