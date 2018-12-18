ca11bb81c7aac9a01c4397fbd6ff557f45d641ad
apescara <apescara@alumnos.uai.cl>
2018-12-18 17:12:09 -0300

    logica
    se agrego la capa de logica para los comprobantes

---
+++ Capstone5/BLogicLayer/comunBL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntitiesLayer;
+using DALayer;
+
+namespace BLogicLayer
+{
+    public class comunBL
+    {
+        #region "SINGLETON"
+        private static comunBL comun = null;
+        private comunBL() { }
+        public static comunBL getInstance()
+        {
+            if (comun == null)
+            {
+                comun = new comunBL();
+            }
+            return comun;
+        }
+        #endregion
+
+
+        public bool RegistrarComprobante(conductor objComp)
+        {
+            try
+            {
+                return comunDA.getInstance().RegistrarComprobante(objComp);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<comun> ListarComprobantes()
+        {
+            try
+            {
+                return comunDA.getInstance().ListarComprobantes();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Eliminar(int id)
+        {
+            try
+            {
+                return comunDA.getInstance().EliminarComprobante(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
