eadb026b99b7960ea4f8f86a8e94230e148e33f6
apescara <apescara@alumnos.uai.cl>
2018-12-18 17:07:45 -0300

    inicio
    se agrego la entidad y el data access

---
+++ Capstone5/DALayer/ComunDA.cs
@@ -0,0 +1,133 @@
+﻿using EntitiesLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALayer
+{
+    public class comunDA
+    {
+        #region "SINGLETON"
+        private static comunDA comun = null;
+        private comunDA() { }
+        public static comunDA getInstance()
+        {
+            if (comun == null)
+            {
+                comun = new comunDA();
+            }
+            return comun;
+        }
+        #endregion
+
+        public bool RegistrarComprobante(comun ObjComp)
+        {
+            SqlConnection con = null;
+            SqlCommand cmd = null;
+            bool response = false;
+            try
+            {
+                con = Connection.getInstance().DBConnection();
+                cmd = new SqlCommand("spRegistrarComprobante", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@prmIdrecepcionista", ObjComp.idRecepcionista);
+                cmd.Parameters.Add("@prmFechaDocumento", ObjComp.fechaDocumento);
+                cmd.Parameters.Add("@prmFechaComprobante", ObjComp.fechaComprobante);
+                cmd.Parameters.Add("@prmIdEmpTransporte", ObjComp.idEmpTransporte);
+                cmd.Parameters.Add("@prmIdRampla", ObjComp.idRampla);
+                cmd.Parameters.Add("@prmIdConductor", ObjComp.idConductor);
+                cmd.Parameters.Add("@prmTemperatura", ObjComp.temperatura);
+                cmd.Parameters.Add("@prmRechazo", ObjComp.rechazo);
+                con.Open();
+                int filas = cmd.ExecuteNonQuery();
+                if (filas > 0) response = true;
+            }
+            catch (Exception ex)
+            {
+                response = false;
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return response;
+        }
+
+        public List<comun> ListarComprobantes()
+        {
+            List<comun> Lista = new List<comun>();
+            SqlConnection con = null;
+            SqlCommand cmd = null;
+            SqlDataReader dr = null;
+            try
+            {
+                con = Connection.getInstance().DBConnection();
+                cmd = new SqlCommand("spListarComprobantes", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    comun objComp = new comun();
+                    objComp.idRecepcionista = Convert.ToInt32(dr["idRecepcionista"]);
+                    objComp.fechaComprobante = Convert.ToDateTime(dr["fechaComprobante"]);
+                    objComp.fechaDocumento = Convert.ToDateTime(dr["fechaDocumento"]);
+                    objComp.idRampla = Convert.ToInt32(dr["idRampla"]);
+                    objComp.idConductor = Convert.ToInt32(dr["idConductor"]);
+                    objComp.idEmpTransporte = Convert.ToInt32(dr["idEmpTransporte"]);
+                    objComp.temperatura = Convert.ToInt32(dr["temperatura"]);
+                    objComp.rechazo = Convert.ToInt32(dr["rechazo"]);
+
+                    Lista.Add(objComp);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return Lista;
+        }
+
+        public bool EliminarComprobante(int id)
+        {
+            SqlConnection con = null;
+            SqlCommand cmd = null;
+            bool ok = false;
+            try
+            {
+                con = Connection.getInstance().DBConnection();
+                cmd = new SqlCommand("spEliminarComprobante", con);
+                cmd.Parameters.AddWithValue("@prmIdComprobante", id);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                ok = true;
+            }
+            catch (Exception ex)
+            {
+                ok = false;
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+
+            return ok;
+        }
+
+    }
+}
---
+++ Capstone5/EntitiesLayer/comun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer
+{
+    public class comun
+    {
+        public int numFolio { get; set; }
+        public int idRecepcionista { get; set; }
+        public DateTime fechaDocumento { get; set; }
+        public DateTime fechaComprobante { get; set; }
+        public int idEmpTransporte { get; set; }
+        public int idRampla { get; set; }
+        public int idConductor { get; set; }
+        public int temperatura { get; set; }
+        public int rechazo { get; set; }
+    }
+}
